5e0d3016168f289946a03418b1df067082491ab2
mawarijo <mdopure@gmail.com>
2018-10-26 10:41:25 -0700

    set up express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('From server with love');
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+module.exports = {
+  app
+};
