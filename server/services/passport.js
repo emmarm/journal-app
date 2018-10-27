49dd40172a681e2ca241a13607db81c5eef26b2e
mawarijo <mdopure@gmail.com>
2018-10-27 09:52:43 -0700

    handle save, serialize, deserialize user

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,7 +1,16 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => done(null, user.id));
+
+passport.deserializeUser((id, done) =>
+  User.findById(id).then(user => done(null, user))
+);
 
 passport.use(
   new GoogleStrategy(
@@ -10,6 +19,16 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    (accessToken, refreshToken, profile, done) => console.log(accessToken)
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (!existingUser) {
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser));
+        }
+
+        done(null, existingUser);
+      });
+    }
   )
 );
