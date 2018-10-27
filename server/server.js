cb1ba44c94b483d4afbddae616b967c3d418002e
mawarijo <mdopure@gmail.com>
2018-10-26 23:28:28 -0700

    set up passport, get /auth/google route working

--- server/server.js
+++ server/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+const keys = require('./config/keys');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.send('From server with love');
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => console.log(accessToken)
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
