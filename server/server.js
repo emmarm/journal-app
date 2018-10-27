2d5822697fad883992554b8175773f5d9c4ea4e9
mawarijo <mdopure@gmail.com>
2018-10-27 01:02:47 -0700

    create User schema

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+require('./models/User');
 require('./services/passport');
 const keys = require('./config/keys');
 
