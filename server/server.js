474c0dc00c46d50e928ee3d875493f77d431309c
mawarijo <mdopure@gmail.com>
2018-10-27 10:05:20 -0700

    add cookie-session and configure

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
 require('./models/User');
 require('./services/passport');
@@ -10,6 +12,14 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
