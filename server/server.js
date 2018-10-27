88fb216349e6062d3497a48a1191d50f6e61991c
mawarijo <mdopure@gmail.com>
2018-10-27 00:38:09 -0700

    refactor file structure

--- server/server.js
+++ server/server.js
@@ -1,35 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-const keys = require('./config/keys');
+require('./services/passport');
 
 const app = express();
-const port = process.env.PORT || 5000;
-
 app.use(bodyParser.json());
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => console.log(accessToken)
-  )
-);
+require('./routes/authRoutes')(app);
 
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
-  })
-);
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 module.exports = {
