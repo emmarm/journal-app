3ba71b42e1559b09ce78bc4eddac1284f98e6160
mawarijo <mdopure@gmail.com>
2018-10-29 23:31:54 -0700

    update isPremium on payment and add requireLogin middleware

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -1,13 +1,20 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
+const requireLogin = require('../middlewares/requireLogin');
+
 module.exports = app => {
-  app.post('/api/payment', async (req, res) => {
+  app.post('/api/payment', requireLogin, async (req, res) => {
     const charge = await stripe.charges.create({
       amount: 999,
       currency: 'USD',
       description: 'Upgrade to Premium',
       source: req.body.id
     });
+
+    req.user.isPremium = true;
+    const user = await req.user.save();
+
+    res.send(user);
   });
 };
