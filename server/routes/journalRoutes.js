9af2ecbb561a1cea412ee9e7055321380f5c4ff8
mawarijo <mdopure@gmail.com>
2018-10-30 23:43:33 -0700

    add route post /api/journals

---
+++ server/routes/journalRoutes.js
@@ -0,0 +1,40 @@
+const requireLogin = require('../middlewares/requireLogin');
+
+const Journal = mongoose.model('journals');
+
+module.exports = app => {
+  app.post('/api/journals', requireLogin, async (req, res) => {
+    const {
+      journalType,
+      affirmation,
+      didWell,
+      gratefulFor,
+      improveTomorrow,
+      learned,
+      motivationalQuote,
+      newWord,
+      promiseToDo,
+      wereGreat,
+      willMakeGreat
+    } = req.body;
+
+    const journal = new Journal({
+      _creator: req.user.id,
+      journalType,
+      affirmation,
+      didWell,
+      gratefulFor,
+      improveTomorrow,
+      learned,
+      motivationalQuote,
+      newWord,
+      promiseToDo,
+      wereGreat,
+      willMakeGreat
+    });
+
+    const savedJournal = await journal.save();
+
+    res.send(savedJournal);
+  });
+};
