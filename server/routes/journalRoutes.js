ab321e1b67a08e4078ea72f3b54e77c604122987
mawarijo <mdopure@gmail.com>
2018-10-31 22:09:28 -0700

    patch and delete journal

--- server/routes/journalRoutes.js
+++ server/routes/journalRoutes.js
@@ -6,33 +6,11 @@ const Journal = mongoose.model('journals');
 
 module.exports = app => {
   app.post('/api/journals', requireLogin, async (req, res) => {
-    const {
-      journalType,
-      affirmation,
-      didWell,
-      gratefulFor,
-      improveTomorrow,
-      learned,
-      motivationalQuote,
-      newWord,
-      promiseToDo,
-      wereGreat,
-      willMakeGreat
-    } = req.body;
+    const { ...fields } = req.body;
 
     const journal = new Journal({
       _creator: req.user.id,
-      journalType,
-      affirmation,
-      didWell,
-      gratefulFor,
-      improveTomorrow,
-      learned,
-      motivationalQuote,
-      newWord,
-      promiseToDo,
-      wereGreat,
-      willMakeGreat
+      ...fields
     });
 
     const savedJournal = await journal.save();
@@ -48,18 +26,73 @@ module.exports = app => {
     res.send(journals);
   });
 
-  app.get('/api/journal/:id', requireLogin, async (req, res) => {
+  app.get('/api/journals/:id', requireLogin, async (req, res) => {
     const { id } = req.params;
 
     const foundJournal = await Journal.findOne({
       _id: id,
       _creator: req.user.id
-    });
+    }).exec();
 
     if (!foundJournal) {
-      res.status(400).send({ error: 'Could not get journal entry.' });
+      return res.status(400).send({ error: 'Could not get journal entry.' });
     }
 
     res.send(foundJournal);
+  });
+
+  app.patch('/api/journals/:id', requireLogin, async (req, res) => {
+    const { id } = req.params;
+    console.log('id: ', id);
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(400).send({ error: 'Could not handle request' });
+    }
+
+    const updates = req.body.filter(field => !!field);
+    console.log('updates: ', updates);
+    const updatedAt = new Date();
+    console.log('updatedAt: ', updatedAt);
+
+    try {
+      const newJournal = await Journal.findOneAndUpdate(
+        { _id: id, _creator: req.user.id },
+        { $set: { ...updates, updatedAt } },
+        { new: true }
+      ).exec();
+
+      if (!newJournal) {
+        return res
+          .status(400)
+          .send({ error: 'Could not update journal entry.' });
+      }
+
+      res.send({ newJournal });
+    } catch (err) {
+      res.status(400).send({ error: 'Something went wrong' });
+    }
+  });
+
+  app.delete('/api/journals/:id', requireLogin, async (req, res) => {
+    const { id } = req.params;
+
+    if (!ObjectID.isValid(id)) {
+      return res.status(400).send({ error: 'Could not handle request' });
+    }
+
+    try {
+      const deletedJournal = await Journal.findOneAndDelete({
+        _id: id,
+        _creator: req.user.id
+      }).exec();
+
+      if (!deletedJournal) {
+        return res.status(404).send();
+      }
+
+      res.send(deletedJournal);
+    } catch (err) {
+      res.status(400).send({ error: 'Could not delete entry.' });
+    }
   });
 };
