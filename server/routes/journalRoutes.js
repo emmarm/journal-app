55e2efeacef7b6a2259e9809def56c434d2571e6
mawarijo <mdopure@gmail.com>
2018-10-31 21:54:34 -0700

    get journals and get single journal by id

--- server/routes/journalRoutes.js
+++ server/routes/journalRoutes.js
@@ -39,4 +39,27 @@ module.exports = app => {
 
     res.send(savedJournal);
   });
+
+  app.get('/api/journals', requireLogin, async (req, res) => {
+    const journals = await Journal.find({
+      _creator: req.user.id
+    }).exec();
+
+    res.send(journals);
+  });
+
+  app.get('/api/journal/:id', requireLogin, async (req, res) => {
+    const { id } = req.params;
+
+    const foundJournal = await Journal.findOne({
+      _id: id,
+      _creator: req.user.id
+    });
+
+    if (!foundJournal) {
+      res.status(400).send({ error: 'Could not get journal entry.' });
+    }
+
+    res.send(foundJournal);
+  });
 };
