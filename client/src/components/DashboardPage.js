35e2255e5e2d06c5b520a46587f42255536acba3
mawarijo <mdopure@gmail.com>
2018-10-31 22:27:29 -0700

    add Link on dashboard page to new journal page

--- client/src/components/DashboardPage.js
+++ client/src/components/DashboardPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DashboardPage = props => (
   <div>
     <h2>My Journal</h2>
+    <Link to="/journal/new">Add Journal</Link>
   </div>
 );
 
