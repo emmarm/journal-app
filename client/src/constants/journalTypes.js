599363502f84bc200c247a83c2af597623509ed2
mawarijo <mdopure@gmail.com>
2018-11-01 23:49:00 -0700

    make DEFAULT journal type constant

---
+++ client/src/constants/journalTypes.js
@@ -0,0 +1,26 @@
+export const DEFAULT = {
+  morning: [
+    {
+      type: 'GRATEFUL_FOR',
+      count: 3
+    },
+    {
+      type: 'WILL_MAKE_GREAT',
+      count: 3
+    },
+    {
+      type: 'AFFIRMATION',
+      count: 1
+    }
+  ],
+  evening: [
+    {
+      type: 'WERE_GREAT',
+      count: 3
+    },
+    {
+      type: 'IMPROVE_TOMORROW',
+      count: 1
+    }
+  ]
+};
