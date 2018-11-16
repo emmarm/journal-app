431d5442ef46e64361d39f38e7ac5a23028f34b3
mawarijo <mdopure@gmail.com>
2018-11-15 22:58:34 -0800

    fix nodemon and server setup

--- server/server.js
+++ server/server.js
@@ -30,14 +30,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   const path = require('path');
-  app.get('/*', (req, res) =>
+  app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   );
 }
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-module.exports = {
-  app
-};
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
